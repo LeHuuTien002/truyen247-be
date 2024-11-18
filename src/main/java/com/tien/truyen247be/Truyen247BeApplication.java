381e9708a3ad641f951edb34fa4506939bca95d5
LeHuuTien002 <lehuutien27022002.com>
2024-11-18 15:59:39 +0700

    favorite and history

--- src/main/java/com/tien/truyen247be/Truyen247BeApplication.java
+++ src/main/java/com/tien/truyen247be/Truyen247BeApplication.java
@@ -1,5 +1,6 @@
 package com.tien.truyen247be;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Truyen247BeApplication {
 
     public static void main(String[] args) {
+        // Đặt các biến môi trường
+        Dotenv dotenv = Dotenv.configure()
+                .directory(System.getProperty("user.dir")) // Chỉ định thư mục chứa .env
+                .load();
+
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
         SpringApplication.run(Truyen247BeApplication.class, args);
     }
 
