96985c78002f9a4b6585d084033250c5e6d4ed4d
LeHuuTien002 <lehuutien27022002.com>
2024-12-23 22:14:27 +0700

    update

---
+++ src/main/java/com/tien/truyen247be/controllers/ComicController.java
@@ -0,0 +1,38 @@
+package com.tien.truyen247be.controllers;
+
+import com.tien.truyen247be.payload.request.ComicRequest;
+import com.tien.truyen247be.security.services.ComicService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api")
+public class ComicController {
+    @Autowired
+    private ComicService comicService;
+
+    @PostMapping("/admin/comics/create")
+    public ResponseEntity<?> createComic(@Valid @RequestPart("comicRequest") ComicRequest comicRequest, @RequestPart("file") MultipartFile file) throws IOException {
+        return ResponseEntity.ok(comicService.createComic(comicRequest, file));
+    }
+
+    @PutMapping("/admin/comics/{id}/update")
+    public ResponseEntity<?> updateComic(@Valid @PathVariable Long id, @RequestPart("comicRequest") ComicRequest comicRequest, @RequestPart(value = "file", required = false) MultipartFile file) throws IOException {
+        return ResponseEntity.ok(comicService.updateComic(id, comicRequest, file));
+    }
+
+    @DeleteMapping("/admin/comics/{id}/delete")
+    public ResponseEntity<?> deleteComic(@PathVariable Long id) {
+        return ResponseEntity.ok(comicService.deleteComic(id));
+    }
+
+    @GetMapping("/admin/comics/list")
+    public ResponseEntity<?> getAllComics() {
+        return comicService.getAllComic();
+    }
+}
